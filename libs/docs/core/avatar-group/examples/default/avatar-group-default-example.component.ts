c1bee25410b82651decf227e5eccb0160eccc4a6
g-cheishvili <giorgi.cheishvili@outlook.com>
2023-07-21 02:51:47 +0400

    feat(docs): added example for the vertical avatar-group and fixed minor focus issue

--- libs/docs/core/avatar-group/examples/default/avatar-group-default-example.component.ts
+++ libs/docs/core/avatar-group/examples/default/avatar-group-default-example.component.ts
@@ -1,8 +1,6 @@
-import { ChangeDetectionStrategy, Component, ViewChild } from '@angular/core';
-import { AvatarGroupComponent } from '@fundamental-ngx/core/avatar-group';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { AvatarGroupDataExampleService } from '../avatar-group-data-example.service';
-import { PopoverBodyComponent } from '@fundamental-ngx/core/popover';
-import { RtlService, Size } from '@fundamental-ngx/cdk/utils';
+import { Size } from '@fundamental-ngx/cdk/utils';
 
 @Component({
     selector: 'fundamental-ngx-avatar-group-default-example',
@@ -10,37 +8,7 @@ import { RtlService, Size } from '@fundamental-ngx/cdk/utils';
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AvatarGroupDefaultExampleComponent {
-    @ViewChild('avatarGroup_IndividualType')
-    avatarGroup: AvatarGroupComponent;
-
-    @ViewChild('overflowPopoverBody')
-    popoverBodyComponent: PopoverBodyComponent;
-
+    readonly avatarGroupDataExampleService: AvatarGroupDataExampleService = inject(AvatarGroupDataExampleService);
     size: Size = 's';
     people = this.avatarGroupDataExampleService.generate();
-    personDetails: any = null;
-    overflowPopoverStage: 'main' | 'detail' = 'main';
-
-    get isDetailStage(): boolean {
-        return this.overflowPopoverStage === 'detail';
-    }
-
-    constructor(
-        private readonly avatarGroupDataExampleService: AvatarGroupDataExampleService,
-        private _rtlService: RtlService
-    ) {}
-
-    get isRtl(): boolean {
-        return this._rtlService.rtl.getValue();
-    }
-
-    openOverflowDetails(person): void {
-        this.personDetails = person;
-        this.overflowPopoverStage = 'detail';
-    }
-
-    openOverflowMain(): void {
-        this.personDetails = null;
-        this.overflowPopoverStage = 'main';
-    }
 }
